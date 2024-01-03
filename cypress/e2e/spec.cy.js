74567e085bf9d5f5e54375e8f1468bc8e9f6d116
emolinazerofox <150184223+emolinazerofox@users.noreply.github.com>
2024-01-03 20:32:18 -0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,7 @@ describe('template spec', { browser: 'firefox' }, () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+  it('passes', { browser: 'electron' }, () => {
+    cy.visit('https://google.com')
+  })
 })
